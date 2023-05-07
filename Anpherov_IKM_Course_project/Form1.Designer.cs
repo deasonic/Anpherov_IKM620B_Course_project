d25d221c860960a9fc95e33f815ca6bff6ee2328
deasonic <anpherov666@gmail.com>
2023-05-07 19:07:58 +0300

    Finish task number 1

--- Anpherov_IKM_Course_project/Form1.Designer.cs
+++ Anpherov_IKM_Course_project/Form1.Designer.cs
@@ -28,18 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            tbInput = new TextBox();
+            label1 = new Label();
+            bStart = new Button();
+            tClock = new System.Windows.Forms.Timer(components);
             SuspendLayout();
+            // 
+            // tbInput
+            // 
+            tbInput.Enabled = false;
+            tbInput.Location = new Point(330, 150);
+            tbInput.Name = "tbInput";
+            tbInput.Size = new Size(100, 23);
+            tbInput.TabIndex = 0;
+            tbInput.KeyPress += tbInput_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(363, 119);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // bStart
+            // 
+            bStart.Location = new Point(339, 254);
+            bStart.Name = "bStart";
+            bStart.Size = new Size(75, 23);
+            bStart.TabIndex = 2;
+            bStart.Text = "Пуск";
+            bStart.UseVisualStyleBackColor = true;
+            bStart.Click += bStart_Click;
+            // 
+            // tClock
+            // 
+            tClock.Interval = 25000;
+            tClock.Tick += tClock_Tick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(813, 432);
+            Controls.Add(bStart);
+            Controls.Add(label1);
+            Controls.Add(tbInput);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox tbInput;
+        private Label label1;
+        private Button bStart;
+        private System.Windows.Forms.Timer tClock;
     }
 }
--- Anpherov_IKM_Course_project/Form1.cs
+++ Anpherov_IKM_Course_project/Form1.cs
@@ -2,9 +2,57 @@ namespace Anpherov_IKM_Course_project
 {
     public partial class Form1 : Form
     {
+        private bool Mode;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show("Минуло 25 секунд", "Увага");
+            tClock.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Mode = true;
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (Mode)
+            {
+                tbInput.Enabled = true;
+                tClock.Start();
+                bStart.Text = "Стоп";
+                this.Mode = false;
+            }
+            else
+            {
+                tbInput.Enabled = false;
+                tClock.Stop();
+                bStart.Text = "Пуск";
+                this.Mode = true;
+            }
+        }
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+            if((e.KeyChar >= '0') & (e.KeyChar <= '9') | (e.KeyChar == (char)8))
+                    {
+                return;
+            }
+            else
+            {
+                tClock.Stop();
+                MessageBox.Show("Неправильний символ", "Помилка");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
         }
     }
 }
